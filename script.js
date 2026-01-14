c44926596858aa1fb70ed2e00b521e987a74073f
Thierry Tavares <thitav09@gmail.com>
2026-01-14 14:38:09 +0100

    Third commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document
+.getElementById("inscriptionForm")
+.addEventListener("submit", function (e) {
+e.preventDefault();
+alert(
+"Merci pour votre demande ! Nous vous contacterons très prochainement pour finaliser votre inscription au camp de ski de Leysin. "
+);
+});
