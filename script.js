748df3c3867245b5afdefcfed04abb4ca9e11902
Thierry Tavares <thitav09@gmail.com>
2026-01-15 13:18:46 +0100

    More Javascript

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document
-.getElementById("inscriptionForm")
-.addEventListener("submit", function (e) {
-e.preventDefault();
-alert(
-"Merci pour votre demande ! Nous vous contacterons très prochainement pour finaliser votre inscription au camp de ski de Leysin. "
-);
+document.getElementById("inscriptionForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Collect data using FormData
+  const form = e.target;
+  const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+
+  // Now 'data' contains all the user's input
+  console.log(data);
+
+  alert(
+    "Merci pour votre demande ! Nous vous contacterons très prochainement" + 
+    "pour finaliser votre inscription au camp de ski de Leysin. "
+  );
 });
